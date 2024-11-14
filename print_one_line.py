40685dfb0ace0cbd645bf38a5637c04f9fd3589e
SEYEON-PARK <fourleaf123@naver.com>
2024-11-14 10:41:11 +0900

    Feat : 리스트에 있는 내용 한 줄로 출력해주는 코드 작성

---
+++ print_one_line.py
@@ -0,0 +1,91 @@
+log = [
+    "alarm_ca_detect_log",
+    "alarm_ca_detect_log_abbrev",
+    "alarm_resource_threshold",
+    "log_aptx_analysis",
+    "log_aptx_detect",
+    "log_aptx_download",
+    "log_aptx_mrfile_analysis",
+    "log_aptx_status_sensor",
+    "log_detect_block",
+    "log_detect_event",
+    "log_traffic_total",
+    "log_traffic_protocol",
+    "log_traffic_service",
+    "log_traffic_frame",
+    "log_traffic_lev_total",
+    "log_traffic_lev_protocol",
+    "log_traffic_lev_service_v2",
+    "log_traffic_lev_frame",
+    "log_traffic_lev_hack",
+    "log_resource_sensor",
+    "log_resource_server",
+    "log_status_sensor",
+    "log_mr_event",
+    "log_mr_reputation",
+    "log_mr_analysis",
+    "stat_ar",
+    "stat_block_method",
+    "stat_block_period",
+    "stat_block_reason",
+    "stat_block_signature",
+    "stat_block_src",
+    "stat_detect_dst",
+    "stat_detect_signature",
+    "stat_detect_src",
+    "stat_ipr",
+    "stat_mr_period",
+    "stat_trf_frame",
+    "stat_trf_mpls",
+    "stat_trf_protocol",
+    "stat_trf_service",
+    "stat_trf_total",
+    "stat_ur" 
+]
+print(log)
+print()
+
+session = [
+    "log_utm_session",
+    "log_utm_ips",
+    "log_utm_ddos",
+    "log_utm_app",
+    "log_utm_ipsec",
+    "log_utm_ipsdump",
+    "log_utm_userauth",
+    "log_utm_sslvpn",
+    "log_utm_audit",
+    "log_utm_system",
+    "log_utm_resource_sensor",
+    "log_utm_traffic_total",
+    "log_utm_antivirus",
+    "log_ngfw_alg",
+    "log_ngfw_as",
+    "log_ngfw_av",
+    "log_ngfw_ddos",
+    "log_ngfw_fw",
+    "log_ngfw_ips",
+    "log_ngfw_ipsec",
+    "log_ngfw_network",
+    "log_ngfw_ssl",
+    "log_ngfw_stat_sensor",
+    "log_ngfw_system",
+    "log_ngfw_traffic_total"
+]
+
+print(session)
+print()
+
+statics = [
+    "stat_tuple_detect_min",
+    "stat_tuple_detect_hour",
+    "stat_tuple_utm_ips_min",
+    "stat_tuple_utm_ddos_min",
+    "stat_tuple_utm_app_min",
+    "stat_tuple_utm_ips_hour",
+    "stat_tuple_utm_ddos_hour",
+    "stat_tuple_utm_app_hour" 
+]
+
+print(statics)
+print()
