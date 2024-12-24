2f14d89446c55a9707e12dded236c915c09bd025
SEYEON-PARK <fourleaf123@naver.com>
2024-12-24 09:40:12 +0900

    Modify : 레드마인에 기존 일감 내용 + 새로운 내용 추가하는 기능 작성 완료(jq 사용하지 않고)

--- auto_daily_check.sh
+++ auto_daily_check.sh
@@ -9,13 +9,7 @@ REDMINE_URL="IP 주소소"
 API_KEY="key"
 
 # 수정할 일감 ID
-ISSUE_ID="일감 번호호"
-
-# 이스케이핑 함수 정의
-escape_json_string() {
-  local str="$1"
-  printf "%s" "$str" | sed -e 's/\\/\\\\/g' -e 's/"/\\"/g' -e 's/\n/\\n/g'
-}
+ISSUE_ID="일감 번호"
 
 # 추가할 내용
 NEW_CONTENT="추가할 내용입니다. 이 내용은 쉘 스크립트를 통해 자동으로 추가되었습니다."
@@ -23,48 +17,50 @@ NEW_CONTENT="추가할 내용입니다. 이 내용은 쉘 스크립트를 통해
 # 기존 내용 불러오기 (예: curl을 사용하거나 Redmine API로 가져옴)
 EXISTING_DESCRIPTION=$(curl -s -X GET "$REDMINE_URL/issues/$ISSUE_ID.json" \
                             -H "X-Redmine-API-Key: $API_KEY" | \
-                            sed -n 's/.*"description":"\([^,]*\)".*/\1/p' | \
-                            sed 's/\\r\\n/\n/g' | sed 's/\\"/\"/g' )
+                            sed -n 's/.*"description":"\([^,]*\)".*/\1/p')
+# | \
+#                            sed 's/\\r\\n/\n/g' | sed 's/\\"/\"/g' )
 
-# 기존 내용과 새로운 내용 합치기
-NEW_DESCRIPTION=$(echo -e "$EXISTING_DESCRIPTION\n$NEW_CONTENT")
+echo "$EXISTING_DESCRIPTION"
 
-# JSON 문자열 이스케이핑
-ESCAPED_DESCRIPTION=$(escape_json_string "$NEW_DESCRIPTION")
 
-# Redmine API 호출
-RESPONSE=$(curl -iv -X PUT "$REDMINE_URL/issues/$ISSUE_ID.json" \
-     -H "Content-Type: application/json" \
-     -H "X-Redmine-API-Key: $API_KEY" \
-     -d "{
-           \"issue\": {
-             \"description\": \"$ESCAPED_DESCRIPTION\"
-           }
-         }")
-
-# 응답 확인
-echo "Response: $RESPONSE"
  
-# echo "$EXISTING_DESCRIPTION"
 # 새로운 내용 추가
 # NEW_DESCRIPTION=$(echo -e "$EXISTING_DESCRIPTION\n$NEW_CONTENT" | sed 's/\n/\\r\\n/g' | sed 's/\"/\\"/g')
+
 # 기존 내용과 새로운 내용 합치기 (줄바꿈을 실제로 처리)
-
 # NEW_DESCRIPTION=$(echo -e "$EXISTING_DESCRIPTION")
-# NEW_DESCRIPTION=$(echo -e "$EXISTING_DESCRIPTION\n$NEW_CONTENT" | sed 's/\n/\\r\\n/g' | sed 's/\"/\\"/g')
+# NEW_DESCRIPTION=$(echo -e "$EXISTING_DESCRIPTION\n$NEW_CONTENT" | sed 's/\n/\\n/g' | sed 's/\"/\\"/g')
+NEW_DESCRIPTION="$EXISTING_DESCRIPTION\r\n$NEW_CONTENT"
 
-# echo
-# echo
-# echo "$NEW_DESCRIPTION"
+# echo "$EXISTING_DESCRIPTION"
+
+echo
+echo
+
+echo "$NEW_DESCRIPTION"
+
+echo
+echo
+
+# JSON 포맷 생성 (줄바꿈 및 따옴표 이스케이프 처리)
+JSON_PAYLOAD=$(printf '{"issue": {"description": "%s"}}' "$NEW_DESCRIPTION")
+echo "$JSON_PAYLOAD"
+
+
 # Redmine API를 통해 일감 내용 추가
-# RESPONSE=$(curl -iv -X PUT "$REDMINE_URL/issues/$ISSUE_ID.json" \
-#     -H "Content-Type: application/json" \
-#     -H "X-Redmine-API-Key: $API_KEY" \
-#     -d '{
-#           "issue": {
-#             "description": "'"$NEW_DESCRIPTION"'"
-#           }
-#         }')
+RESPONSE=$(curl -iv -X PUT "$REDMINE_URL/issues/$ISSUE_ID.json" \
+    -H "Content-Type: application/json" \
+    -H "X-Redmine-API-Key: $API_KEY" \
+    -d "$JSON_PAYLOAD")
+
+
+
+#    -d "{
+#          \"issue\": {
+#            \"description\": \"$NEW_DESCRIPTION\"
+#          }
+#        }")
 
 # 응답 확인
 echo "Response: $RESPONSE"
